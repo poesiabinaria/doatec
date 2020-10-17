f6f1e38b8eee516280e0438de9420e02088af5ab
hitallocavas <hcs3@cin.ufpe.br>
2020-10-17 19:54:36 -0300

    ajustes backend

--- src/components/CadastrarInstituicao.js
+++ src/components/CadastrarInstituicao.js
@@ -9,67 +9,85 @@ import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 
 function CadastrarInstituicao() {
-  const [nome, setNome] = useState()
-  const [email, setEmail] = useState()
-  const [senha, setSenha] = useState()
+    const [name, setNome] = useState()
+    const [mail, setEmail] = useState()
+    const [password, setSenha] = useState()
 
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    console.log(nome)
-    try {
-      const response = await api.post("URL DO SERVIDOR", {
-        nome,
-        email,
-        senha,
-      })
-      console.log(response.data)
-    } catch (e) {
-      console.log("Erro")
+    async function handleSubmit(e) {
+        e.preventDefault()
+        try {
+            const response = await api.post("auth/register", {
+                name,
+                mail,
+                password,
+                role: 'INSTITUICAO'
+            })
+            console.log(response.data)
+        } catch (e) {
+            console.log("Erro")
+        }
     }
-  }
 
-  return (
-    <Page title="Cadastre-se como uma instituição">
-      <header className="header-interno">
-        <h4>Cadastre-se como uma instituição</h4>
-      </header>
+    return ( <
+        Page title = "Cadastre-se como uma instituição" >
+        <
+        header className = "header-interno" >
+        <
+        h4 > Cadastre - se como uma instituição < /h4> < /
+        header >
 
-      <div className="conteudo-interno">
-        <Row>
-          <Col lg="8">
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Control
-                  onChange={(e) => setNome(e.target.value)}
-                  type="text"
-                  placeholder="Nome da instituiçao"
-                />
-              </Form.Group>
+        <
+        div className = "conteudo-interno" >
+        <
+        Row >
+        <
+        Col lg = "8" >
+        <
+        Form onSubmit = { handleSubmit } >
+        <
+        Form.Group controlId = "formBasicEmail" >
+        <
+        Form.Control onChange = {
+            (e) => setNome(e.target.value)
+        }
+        type = "text"
+        placeholder = "Nome da instituiçao" /
+        >
+        <
+        /Form.Group>
 
-              <Form.Group controlId="formBasicEmail">
-                <Form.Control
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  placeholder="E-mail"
-                />
-              </Form.Group>
+        <
+        Form.Group controlId = "formBasicEmail" >
+        <
+        Form.Control onChange = {
+            (e) => setEmail(e.target.value)
+        }
+        type = "email"
+        placeholder = "E-mail" /
+        >
+        <
+        /Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Control
-                  onChange={(e) => setSenha(e.target.value)}
-                  type="password"
-                  placeholder="Senha"
-                />
-              </Form.Group>
+        <
+        Form.Group controlId = "formBasicPassword" >
+        <
+        Form.Control onChange = {
+            (e) => setSenha(e.target.value)
+        }
+        type = "password"
+        placeholder = "Senha" /
+        >
+        <
+        /Form.Group>
 
-              <Button type="submit">Cadastrar</Button>
-            </Form>
-          </Col>
-        </Row>
-      </div>
-    </Page>
-  )
+        <
+        Button type = "submit" > Cadastrar < /Button> < /
+        Form > <
+        /Col> < /
+        Row > <
+        /div> < /
+        Page >
+    )
 }
 
 export default CadastrarInstituicao
